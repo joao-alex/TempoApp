7b82596f92f96dc567e061959eef24e5042ae82e
Joao Alex <44981295+joao-alex@users.noreply.github.com>
2019-09-23 20:58:39 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,110 @@
+
+
+import React, {Fragment,useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  StatusBar,
+  ImageBackground,
+  TextInput
+} from 'react-native';
+
+import Geolocation from '@react-native-community/geolocation'
+
+
+import bg from './src/assets/bg.png'
+
+const App = () => {
+
+  const [latitide,setLatitude] = useState("");
+  const [longitude,setLongitude] = useState("")
+  const [city,setCity] = useState("")
+
+  function findLocation(){
+    Geolocation.getCurrentPosition(position=>{
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      },error=> alert(error.message),{timeout:20000,maximumAge:1000}
+    );
+  }
+
+  function openWeather(op){
+    if(op==0)
+      alert(city)
+    else{
+      findLocation()
+      alert(latitide)
+    }
+  }
+
+  return (
+    <Fragment>
+      <StatusBar barStyle="white-content" backgroundColor="#7087D2"/>
+      <SafeAreaView>
+        <ImageBackground source={bg} style={styles.back}>
+          <TextInput placeholder="Informe a cidade" style={styles.input} 
+            onChangeText={setCity}
+            value={city}
+            placeholderTextColor="#dff" 
+            returnKeyType="search" 
+            onSubmitEditing={()=>{openWeather(0)}}/>
+          <Text style={styles.t1}>OU</Text>
+          <TouchableOpacity style={styles.b1} onPress={()=>{openWeather(1)}}>
+            <Text style={styles.t2}>Minha Localização</Text>
+          </TouchableOpacity>
+          <Text>{latitide}</Text>
+        </ImageBackground>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  back:{
+    width: '100%', 
+    height: '100%',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  input:{
+    color:"#dff",
+    width:"80%",
+    height:60,
+    paddingLeft:10,
+    borderColor:'#dff',
+    borderWidth:2,
+    borderRadius:10,
+    fontSize:25,
+    textAlign:'center',
+    fontWeight:'bold'
+  },
+  t1:{
+    fontSize:30,
+    paddingTop:30,
+    fontWeight:'bold',
+    color:'#dff',
+    paddingBottom:30,
+  },
+  b1:{
+    backgroundColor:"#dff",
+    alignItems:'center',
+    justifyContent:'center',
+    width:"80%",
+    height:60,
+    borderColor:'#dff',
+    borderWidth:2,
+    borderRadius:10,
+  },
+  t2:{
+    fontSize:30,
+    paddingTop:30,
+    fontWeight:'bold',
+    color:'#57ACE5',
+    paddingBottom:30,
+  },
+
+});
+
+export default App;
